ac1b4ae8ab82bf2c907e99c37744700158915833
rudrayanikanmande24-hash <rudrayanikanmande24@gmail.com>
2026-03-13 11:39:09 +0530

    done

--- src/app/shared/component/navbar/navbar.component.ts
+++ src/app/shared/component/navbar/navbar.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
-
+  
+  isOpen=false
   constructor() { }
 
   ngOnInit(): void {
